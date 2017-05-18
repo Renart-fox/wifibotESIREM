bfc44230f00a1d306bc0854dcffacbc72a36b674
Baptfist <paulmier.baptiste@gmail.com>
2017-05-18 12:03:54 +0200

    Udp code

--- wifibotKevinBaptiste/udpclient.h
+++ wifibotKevinBaptiste/udpclient.h
@@ -8,9 +8,20 @@ class UDPClient : public QObject
 {
 Q_OBJECT
 public:
-    UDPClient();
+    UDPClient(QString IP="192.168.1.106", int port=15000);
+
+    void readPendingDatagrams();
+    void processTheDatagram(QNetworkDatagram datagram);
+
 private:
-    QUdpSocket *socket;
+
+    void setup(QString IP, int port);
+    QString wifibotIP;
+    int wifibotPort;
+    QUdpSocket *udpsocket;
+
+private slots:
+    void connectionEstablished();
 };
 
 #endif // UDPCLIENT_H
